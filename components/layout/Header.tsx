0f1ce67d0bc24d8917d007ec4db7b4b57cb53fda
OmarZalat <zaltomar18@gmail.com>
2025-07-07 11:55:16 +0300

    resize header logo image icon

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -56,8 +56,8 @@ export default function Header() {
             <Image
               src="/assets/images/Logo AC.jpeg"
               alt="Company Logo"
-              width={200}
-              height={80}
+              width={150}
+              height={70}
             />
           </Link>
         </div>
@@ -159,8 +159,8 @@ export default function Header() {
               <Image
                 src="/assets/images/Logo AC.jpeg"
                 alt="Company Logo"
-                width={150}
-                height={60}
+                width={100}
+                height={40}
               />
             </Link>
           </div>
